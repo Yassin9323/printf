d03d393619dc4c416b35fc365e02edb3ee9658f4
root <root@Alpha>
2023-10-15 23:07:54 +0300

    edit step 2

--- main.h
+++ main.h
@@ -4,7 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdrag.h>
-
+//step 1
 int _printf(const char *format, ...);
+
+//step 2 
+print_char(va_list ap , params_t * params );
+print_percent(va_list ap , params_t * params );
+print_string(va_list ap , params_t * params );
 
 #endif
